095b4ab3c154ed0621a1f2f19d9aa6be065070e6
Alejandro Iglesias <alexiglesias3110@gmail.com>
2021-08-14 23:17:11 -0400

    Add jquery to local public

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,15 @@
 
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
+const router = require('express').Router();
 
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+
+  router.route('/api/convert')
+    .get((req,res) => {
+      console.log(req.query);
+    })
 
 };
